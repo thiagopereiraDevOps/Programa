f79b319ac1d61ca0339bfc812595f9af3fb316c3
Thiago Pereira Da Silva <thiago.silva@venturus.org.br>
2023-03-13 15:16:59 -0300

    Meus arquivos de programa

---
+++ src/Programa.java
@@ -0,0 +1,10 @@
+public class Programa {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+
+        int x = (~a & b);
+
+        System.out.println("x = " + x);
+    }
+}
